fc71e98fb6276d31bec5d3017c2ed9bcdf77594c
00134arun.s@gmail.com <84963826+arunsaseendran@users.noreply.github.com>
2025-09-21 12:57:52 +0530

    update

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL='http://localhost:4000/api/v1'
+// export const BASE_URL='http://localhost:4000/api/v1'
+export const BASE_URL = "https://wanderbloom-backend.onrender.com/api/v1";
